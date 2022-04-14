8191807fac3ca44a5fd90baa1e385d7909c701b2
Punk <103642623+RealPunk@users.noreply.github.com>
2022-04-14 19:24:49 +0000

    Add status of install

--- install.bat
+++ install.bat
@@ -1,2 +1,12 @@
+@echo off
+
+echo Status: Installing Python Modules
+echo Installing module colorama
 pip install colorama
+cls
+echo Status: Installing Python Modules
+echo Installing module pyfiglet
 pip install pyfiglet
+cls
+echo All modules installed.
+echo Completed! Please run the main program with the command in the README file!
